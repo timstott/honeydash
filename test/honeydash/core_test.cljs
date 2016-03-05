8ef1e480b8d832bcab582226aa19c7ec501311df
Timothy Stott <stott.timothy@gmail.com>
2016-03-05 10:39:15 +0000

    Project boilerplate with tests

---
+++ test/honeydash/core_test.cljs
@@ -0,0 +1,6 @@
+(ns honeydash.core-test
+  (:require [cljs.test :refer-macros [deftest is testing]]
+            [honeydash.core :as core]))
+
+(deftest test-numbers
+  (is (= 1 4)))
---
+++ test/honeydash/test.cljs
@@ -0,0 +1,9 @@
+(ns honeydash.test
+  (:require [honeydash.core-test]
+            [cljs.test :refer-macros [run-all-tests]]))
+
+(enable-console-print!)
+
+(defn ^:export run
+  []
+  (run-all-tests #"honeydash.*-test"))
