3882092cad486b4f2fdc65d4ced7a1c3d75714f1
Timothy Stott <stott.timothy@gmail.com>
2016-03-13 20:52:33 +0000

    Licensed :hammer:

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject honeydash "0.1.0"
-  :description "FIXME: write this!"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A Honeybadger dashboard for teams"
+  :url "http://github.com/timstott/honeydash"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
 
   :min-lein-version "2.5.3"
 
